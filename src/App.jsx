1b62e6865eb7b5ca4dab8a651f041db1fb6370b5
ordoñez dante martin <dantemartinordonez@hotmail.com>
2023-09-05 19:39:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import './component/NavBar'
+
+function App() {
+
+  return (
+    <div className='container'>
+<NavBar/>
+<ItemListContainer greeting="saludo"/>
+    </div>
+  )
+}
+
+export default App
